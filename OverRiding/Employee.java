b3c6455eedc6800c27c0f1384f7ea72cb6c11d08
Md. Ashraful Alam Akif <87029145+AshrafulAlamAkif@users.noreply.github.com>
2023-04-03 14:58:49 +0600

    Add files via upload

---
+++ OverRiding/Employee.java
@@ -0,0 +1,30 @@
+package OverRiding;
+
+public class Employee extends Person {
+	private String employeeId;
+	private String title;
+	
+	public Employee() {
+		super("Datch");
+		System.out.println("this employee default constr");
+	}
+	
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setgetEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	//this method is Parent class
+	public void setName(String name) {
+		System.out.println(name + "****");
+	}
+
+}
---
+++ OverRiding/test.java
@@ -0,0 +1,16 @@
+package OverRiding;
+
+public class test {
+
+	public static void main(String[] args) {
+		Employee empl = new Employee();
+		Person prsn = new Person();
+		
+		empl.setName("Karim");
+		prsn.setName("joshim");
+		//print statement is given parents class -> Parson)  
+//		System.out.println(empl.getName());
+//		System.out.println(prsn.getName());
+	}
+
+}
